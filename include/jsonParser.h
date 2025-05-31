e98cd1a9146612e2db264648246f43732a4cd55f
Amiya55 <desf95599@163.com>
2025-05-31 21:14:06 +0800

    添加了Json语法检查模块

--- include/jsonParser.h
+++ include/jsonParser.h
@@ -1,32 +1,46 @@
 #ifndef JSONPARSER_H
 #define JSONPARSER_H
 
-#include <stdint.h>
 #include <fstream>
-#include <stack>
 #include <string>
 #include "jsonTypes.h"
 
+class JsonSyntaxCheker {
+public:
+    JsonSyntaxCheker() = default;
+    ~JsonSyntaxCheker() = default;
+
+    void syntax_check(
+        const std::string& jsonStr,
+        JsonTypeName containerType); // may throw exception: runtime_error
+
+private:
+    // check the key in json object
+    void _check_key(
+        const std::string& key); // may throw exception: runtime_error
+    // check the value in json object, also it can check the element in json array
+    void _check_value(
+        const std::string& value); // may throw exception: runtime_error
+};
+
 class JsonFile {
-   public:
+public:
     JsonFile();
     ~JsonFile();
 
-    void open_json(const char* fileName);  // may throw exception: runtime_error
+    void open_json(const char* fileName); // may throw exception: runtime_error
     void close_json() noexcept;
     bool is_open() noexcept;
 
-   private:
+private:
     std::string _fileName;
     std::fstream _file;
 
-    std::string _jsonStr;  // the json data, raw str data from the .json file
-    JsonType* _jsonData;   // the json data, maybe json object or json array
+    std::string _jsonStr; // the json data, raw str data from the .json file
+    JsonType* _jsonData; // the json data, maybe json object or json array
 
+    void _load_data(); // may throw exception: runtime_error
     // check the json syntax, if there is a syntax problem, throw the exception
-    void _check_syntax();  // may throw exception: runtime_error
-
-    void _load_data();  // may throw exception: runtime_error
 };
 
 #endif  // JSONPARSER_H
