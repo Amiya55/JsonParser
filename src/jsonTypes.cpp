6fb8f083ff0c23cde9cd40432bcf436418f098a2
Amiya55 <desf95599@163.com>
2025-06-09 18:58:40 +0800

    修复了词法分析器中的123e-3无法解析的bug，添加了语法分析器的基本雏形

--- src/jsonTypes.cpp
+++ src/jsonTypes.cpp
@@ -1,73 +1,166 @@
 #include "jsonTypes.h"
 
+namespace simpleJson {
+#if __cplusplus >= 201703L
+    JsonValue::JsonValue()
+        : _type(JsonType::Null)
+        , _value(nullptr) {}
 
-/* ---JsonInt--- */
-JsonInt::JsonInt(long long value) : _int(value) {}
+    JsonValue::JsonValue(int value)
+        : _type(JsonType::Int)
+        , _value(value) {}
 
-bool JsonInt::operator==(const JsonInt &obj) const { return _int == obj._int; }
-bool JsonInt::operator==(long long value) const { return _int == value; }
+    JsonValue::JsonValue(double value)
+        : _type(JsonType::Float)
+        , _value(value) {}
 
-JsonInt::Type JsonInt::type() const { return Type::Int; }
+    JsonValue::JsonValue(bool value)
+        : _type(JsonType::Bool)
+        , _value(value) {}
+
+    JsonValue::JsonValue(const std::string& value)
+        : _type(JsonType::String)
+        , _value(value) {}
+
+    JsonValue::JsonValue(const std::vector<JsonValue> &value)
+        : _type(JsonType::Array)
+        , _value(value) {}
+
+    JsonValue::JsonValue(const std::unordered_map<std::string, JsonValue> &value)
+        : _type(JsonType::Object)
+        , _value(value) {}
+
+    JsonType JsonValue::getType() const {
+        return _type;
+    }
+
+#else  // _cplusplus >= 201703L
+    JsonValue::JsonData::JsonData() {
+        // nothing to do
+    }
+
+    JsonValue::JsonData::~JsonData() {
+        // nothing to do
+    }
+
+    JsonValue::JsonValue()
+        : _type(JsonType::Null) {
+        _data._null = nullptr;
+    }
+
+    JsonValue::JsonValue(int value)
+        : _type(JsonType::Int) {
+        _data._int = value;
+    }
+
+    JsonValue::JsonValue(double value)
+        : _type(JsonType::Float) {
+        _data._float = value;
+    }
+
+    JsonValue::JsonValue(bool value)
+        : _type(JsonType::Bool) {
+        _data._bool = value;
+    }
+
+    JsonValue::JsonValue(const std::string &value)
+        : _type(JsonType::String) {
+        new(&_data._string) std::string(value);
+    }
+
+    JsonValue::JsonValue(const std::vector<JsonValue> &value)
+        : _type(JsonType::Array) {
+        new(&_data._array) std::vector<JsonValue>(value);
+    }
+
+    JsonValue::JsonValue(const std::unordered_map<std::string, JsonValue> &value)
+        : _type(JsonType::Object) {
+        new(&_data._object) std::unordered_map<std::string, JsonValue>(value);
+    }
+
+    JsonValue::JsonValue(const JsonValue &value) {
+        _type = value._type;
+        switch (value._type) {
+            case JsonType::Null:
+                // nothing to do
+                break;
+            case JsonType::Int:
+                _data._int = value._data._int;
+                break;
+            case JsonType::Float:
+                _data._float = value._data._float;
+                break;
+            case JsonType::Bool:
+                _data._bool = value._data._bool;
+                break;
+            case JsonType::Object:
+                new(&_data._object) std::unordered_map<std::string, JsonValue>(value._data._object);
+                break;
+            case JsonType::Array:
+                new(&_data._array) std::vector<JsonValue>(value._data._array);
+                break;
+            case JsonType::String:
+                new(&_data._string) std::string(value._data._string);
+                break;
+            default:
+                ;
+        }
+    }
+
+    JsonValue::~JsonValue() {
+        switch (_type) {
+            case JsonType::Object:
+                _data._object.~unordered_map();
+                break;
+            case JsonType::Array:
+                _data._array.~vector();
+                break;
+            case JsonType::String:
+                _data._string.~basic_string();
+                break;
+            default:
+                ;
+        }
+    }
+
+    JsonValue &JsonValue::operator=(const JsonValue &value) {
+        if (this == &value) {
+            this->~JsonValue();
+        }
+
+        _type = value._type;
+        switch (value._type) {
+            case JsonType::Null:
+                // nothing to do
+                break;
+            case JsonType::Int:
+                _data._int = value._data._int;
+                break;
+            case JsonType::Float:
+                _data._float = value._data._float;
+                break;
+            case JsonType::Bool:
+                _data._bool = value._data._bool;
+                break;
+            case JsonType::String:
+                new(&_data._string) std::string(value._data._string);
+                break;
+            case JsonType::Array:
+                new(&_data._array) std::vector<JsonValue>(value._data._array);
+                break;
+            case JsonType::Object:
+                new(&_data._object) std::unordered_map<std::string, JsonValue>(value._data._object);
+                break;
+            default:
+                ;
+        }
+
+        return *this;
+    }
 
 
-/* ---JsonFlt--- */
-JsonFlt::JsonFlt(double value) : _flt(value) {}
-
-bool JsonFlt::operator==(const JsonFlt &obj) const { return _flt == obj._flt; }
-bool JsonFlt::operator==(double value) const { return _flt == value; }
-
-JsonFlt::Type JsonFlt::type() const { return Type::Float; }
-
-/* ---JsonBool--- */
-JsonBool::JsonBool(bool value) : _flag(value) {}
-
-bool JsonBool::operator==(const JsonBool &obj) const { return _flag == obj._flag; }
-bool JsonBool::operator==(bool value) const { return _flag == value; }
-
-JsonBool::Type JsonBool::type() const { return Type::Bool; }
-
-/* ---JsonStr--- */
-JsonStr::JsonStr(const std::string &value) : _str(value) {}
-
-const std::string &JsonStr::string() const { return _str; }
-
-void JsonStr::push(const JsonStr &obj) { _str.append(obj._str); }
-void JsonStr::push(const char *str) { _str.append(str); }
-
-bool JsonStr::operator==(const JsonStr &obj) const { return _str == obj._str; }
-
-bool JsonStr::operator==(const std::string &value) const { return _str == value; }
-
-bool JsonStr::operator==(const char *value) const { return _str.c_str() == value; }
-
-char &JsonStr::operator[](size_t pos) { return _str[pos]; }
-
-JsonStr::Type JsonStr::type() const { return Type::String; }
-size_t JsonStr::size() const { return _str.size(); }
-
-
-/* ---JsonObj--- */
-JsonObj::JsonObj() {}
-
-JsonObj::Type JsonObj::type() const { return Type::Object; }
-size_t JsonObj::size() const { return _dict.size(); }
-
-JsonValue *JsonObj::operator[](const JsonStr &key) { return _dict[key.string()]; }
-JsonValue *JsonObj::operator[](const std::string &key) { return _dict[key]; }
-JsonValue *JsonObj::operator[](const char *key) { return _dict[key]; }
-
-void JsonObj::push(const std::pair<std::string, JsonValue *> &pair) {}
-
-
-/* ---JsonArr--- */
-JsonArr::JsonArr() {}
-
-JsonArr::Type JsonArr::type() const { return Type::Array; }
-size_t JsonArr::size() const { return _arr.size(); }
-
-JsonValue *JsonArr::operator[](size_t pos) { return _arr[pos]; }
-
-/* ---JsonNull--- */
-JsonNull::JsonNull() {}
-
-JsonNull::Type JsonNull::type() const { return Type::Null; }
+    JsonType JsonValue::getType() const {
+        return _type;
+    }
+#endif  // _cplusplus >= 201703L
+}
