f4e8231ca87fc1fda8e7381a9cc17a6e43aa74f9
Amiya55 <suimiaomiaomiao@163.com>
2025-07-20 22:58:39 +0800

    添加了词法分析器Lexer的雏形

--- include/Config.h
+++ include/Config.h
@@ -2,11 +2,14 @@
 #define CONFIG_H
 
 namespace simpleJson {
-
 #define ERR_TYPE_MISMATCH "The type mismatch occured "  // 错误提示：类型不匹配
 
-#define TOKEN_POS_T unsigned long long  // 关于某个token定位的数据类型
+#define POS_T unsigned long long  // 关于某个token定位的数据类型
+#define LENGTH_T unsigned long long  // 某个Token的长度的类型(Token长度以字符数为准)
 
+    // 只允许字符串，包括字符串字面量，字符和std::string
+    template<typename T>
+    using enableIfString = std::enable_if_t<std::is_constructible_v<std::string, T> >;
 } // namespace simpleJson
 
 #endif // CONFIG_H
